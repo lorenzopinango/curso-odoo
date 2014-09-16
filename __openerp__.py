ed8d9d064e903670c2f463666f305f754d4ad549
Lorenzo Piñango <lorenzopinango@example.com>
2014-09-16 11:06:54 -0430

    Agregar archivos para declarar modulo

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+{
+    'name' : 'Curso OpenErp',
+    'description' : 'Este módulo es para aprender OpenErp',
+    'author' : 'Lorenzo Piñango',
+    'version' : 'dia1',
+    'depends' : ['base', ],
+    'data' : [],
+    'demo' : [],
+}
