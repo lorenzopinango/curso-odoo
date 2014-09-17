ea053b90bb1c638b00a496f1e23fa14d74c1d439
Lorenzo Piñango <lorenzopinango@gmail.com>
2014-09-17 18:05:11 -0430

    Se modificaron los modelos

--- models/categoria.py
+++ models/categoria.py
@@ -6,9 +6,10 @@ from openerp.osv import osv, fields
 class categoria(osv.osv):
     _name = 'co.categoria'
     _description = 'CO Categoria'
+    _rec_name = 'name'
 
     _columns = {
-        'name' : fields.char('Nombre'),
+        'name' : fields.char('Nombre' , required=True),
         'description' : fields.text('Descripción'),
         'parent_id' : fields.many2one('co.categoria', 'Padre'),
         'child_ids' : fields.one2many(
--- models/suscripcion.py
+++ models/suscripcion.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp.osv import osv, fields
+from datetime import datetime
 
 TIPOS = [
     ('oro', 'Plan ORO'),
@@ -12,15 +13,30 @@ TIPOS = [
 class suscripcion(osv.osv):
     _name = 'co.suscripcion'
     _description = 'CO Suscripcion'
+    _rec_name = 'code'
+    _order = 'date_start desc'
 
     _columns = {
         'code' : fields.char('Código'),
-        'type' : fields.selection(TIPOS, 'Tipo de suscripción'),
-        'date_star' : fields.date('Inicio suscripción'),
-        'date_end' : fields.date('Fin suscripción'),
-        'active' : fields.boolean('Activo'),
-        'suscriptor_id': fields.many2one('co.suscriptor', 'Afiliado'),
+        'type' : fields.selection(TIPOS, 'Tipo de suscripción', required=True),
+        'date_start' : fields.date('Inicio suscripción', required=True),
+        'date_end' : fields.date('Fin suscripción', required=True),
+        'active' : fields.boolean('Activo', required=True),
+        'suscriptor_id': fields.many2one('co.suscriptor', 'Afiliado', required=True),
     }
+
+    _defaults = {
+        'active' : True,
+        'date_start' : datetime.now().strftime('%Y-%m-%d'),
+    }
+
+    def create(self, cr, uid, values, context=None):
+        if context is None:
+            context = {}
+            
+        values.update({
+            'code': self.pool.get('ir.sequence').get(cr, uid, 'seq.suscripcion')})
+        return super(suscripcion, self).create(cr, uid, values, context=context)
 
 suscripcion()
 
