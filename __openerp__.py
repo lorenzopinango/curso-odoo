2d14cdb8164c2397c93a78a64df993e2a668b8d7
Lorenzo Piñango <lorenzopinango@gmail.com>
2014-09-18 18:27:32 -0430

    Agregamos permisos de seguridad y perfiles

--- __openerp__.py
+++ __openerp__.py
@@ -8,6 +8,7 @@
     'version' : 'dia1',
     'depends' : ['base', 'mail', ],
     'data' : [
+        'security/curso_odoo_security.xml',
         'views/curso_odoo_view.xml',
         'views/multimedia_view.xml',
         'views/tipo_medio_view.xml',
@@ -16,7 +17,9 @@
         'views/suscripcion_view.xml',
         'views/solicitud_view.xml',
         'views/categoria_view.xml',
+        'security/menu_security.xml',
         'data/suscripcion_data.xml',
+        'security/ir.model.access.csv',
     ],
     'demo' : [],
 }
